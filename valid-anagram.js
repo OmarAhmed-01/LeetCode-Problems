a684655bf11c4b5da8bb4e1ef73e891ca81e7e36
Omar Ahmed <omarahmed19991@outlook.com>
2024-07-31 15:02:45 +0300

    adjust latest problem

--- valid-anagram.js
+++ valid-anagram.js
@@ -10,5 +10,25 @@ var isAnagram = function(s, t) {
     let sHashmap = {};
     let tHashmap = {};
 
-    
+    for(let i = 0; i < s.length; i++){
+        if(!sHashmap[s[i]]){
+            sHashmap[s[i]] = 1;
+        }
+        else{
+            sHashmap[s[i]] += 1;
+        }
+        if(!tHashmap[t[i]]){
+            tHashmap[t[i]] = 1;
+        }
+        else{
+            tHashmap[t[i]] += 1;
+        }
+    }
+
+    for (const key in sHashmap) {
+        if(sHashmap[key] != tHashmap[key]){
+            return false;
+        }
+    }
+    return true;
 };
