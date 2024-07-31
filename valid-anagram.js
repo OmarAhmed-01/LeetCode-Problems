66038c36cad92e0a5d931fcedc399e547e7f3e96
Omar Ahmed <omarahmed19991@outlook.com>
2024-07-31 14:37:27 +0300

    adding new problems

---
+++ valid-anagram.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    if(s.length != t.length){
+        return false;
+    }
+    let sHashmap = {};
+    let tHashmap = {};
+
+    
+};
